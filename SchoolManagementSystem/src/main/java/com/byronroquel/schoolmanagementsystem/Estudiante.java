88ddda73b98aca01529229fb4e2f5de5448465cc
broquel-2022168 <broquel-2022168@kinal.edu.gt>
2024-09-11 14:52:29 -0600

    Se han creado las clases de Curso, Estudiante y Gestor academico

---
+++ SchoolManagementSystem/src/main/java/com/byronroquel/schoolmanagementsystem/Estudiante.java
@@ -0,0 +1,61 @@
+package com.byronroquel.schoolmanagementsystem;
+/**
+ *
+ * @author LENOVO
+ */
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String fechaDeNacimiento;
+    public String estado;
+    
+    public Estudiante (int id, String nombre, String apellido, String fechaDeNacimiento, String estado){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+    
+    public String getEstado(){
+        return estado;
+    }
+    
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+     
+}
---
+++ SchoolManagementSystem/src/main/java/com/byronroquel/schoolmanagementsystem/GestorAcademico.java
@@ -0,0 +1,16 @@
+package com.byronroquel.schoolmanagementsystem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class GestorAcademico {
+    private ArrayList<Estudiante> estudiante = new ArrayList<>();
+    private ArrayList<Curso> curso = new ArrayList<>();
+    private HashMap <Integer, ArrayList<Estudiante>> estudiantePorCurso = new HashMap<>();
+    
+    
+}
